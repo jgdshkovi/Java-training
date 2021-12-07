63e58e71b4cb04613ec6cc423b6aba89b8a0a16a
Jagadeesh <41447042+jgdshkovi@users.noreply.github.com>
2021-12-07 15:06:47 +0530

    Add files via upload

---
+++ Exception6.java
@@ -0,0 +1,28 @@
+package com.Exceptions;//our class should extends exception,Throwable
+//parameterized constructor
+class AgeNotValidException extends Exception//RunTimeException,Throwable
+{
+	private int age;
+	public AgeNotValidException(int age) 
+	{
+		this.age=age;
+	}
+	public String toString() {
+		return "you are not eligable for vote"+age;
+	}}
+public class Exception6 
+{
+	static void validation(int age) throws AgeNotValidException  
+       {
+     if(age<18)
+    	
+        throw new AgeNotValidException(age);//MyException1 obj
+     else
+      System.out.println("you are eligable to vote");
+   }  
+   public static void main(String args[]) throws AgeNotValidException 
+      {
+				Exception6.validation(16);
+      System.out.println("rest of the code...");
+      }
+   }
